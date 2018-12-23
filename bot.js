38bbad137444901e585e394150a5579c0865c3f8
hasanhattab12 <45760305+hasanhattab12@users.noreply.github.com>
2018-12-23 21:05:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "c";
 var adminprefix = '-'
 
-const developers = ["472413769700474901"]
+const developers = ["491906076258598912"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
